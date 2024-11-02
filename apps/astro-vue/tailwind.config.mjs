8ffac0d267ed16000d48971460233e877d92ffd3
kerem <keremtiryaki@gmail.com>
2024-11-01 20:22:15 -0400

    astro and vite projects are using same ui package 🥹

--- apps/astro-vue/tailwind.config.mjs
+++ apps/astro-vue/tailwind.config.mjs
@@ -8,6 +8,7 @@ export default {
   
     content: [
       './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+      './node_modules/shadcn-vue/src/**/*.{ts,tsx,vue}',
     ],
   
   theme: {
